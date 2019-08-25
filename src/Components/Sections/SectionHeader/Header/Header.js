2d5bef2a6ea29d90f3d907962f2ec73aae84aaaa
martinzimmerdesign <m.zimmer.business@gmail.com>
2019-08-25 16:49:46 +0200

    0.1.7

--- src/Components/Sections/SectionHeader/Header/Header.js
+++ src/Components/Sections/SectionHeader/Header/Header.js
@@ -9,13 +9,6 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
 export default class HeaderSection extends React.Component {
 
   componentDidMount() {
